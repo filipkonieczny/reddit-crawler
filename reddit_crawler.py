2d31ce05dd141856bfa8f68acf8ad612dd93733a
jhgrng <jhgrng@gmail.com>
2015-01-30 11:01:43 +0100

    Created reddit_crawler script skeleton

---
+++ reddit_crawler.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+# TODO: description
+# description
+
+
+# imports
+
+
+# constants
+
+
+# classes
+class RedditCrawler():
+    # TODO: description
+    '''
+    '''
+
+    def __init__(self):
+        # TODO: description
+        '''
+        '''
+
+        pass
+
+    def crawl(self):
+        # TODO: description
+        '''
+        '''
+
+        pass
+
+    def crawl_page(self):
+        # TODO: description
+        '''
+        '''
+
+        pass
+
+    def return_data(self):
+        # TODO: description
+        '''
+        '''
+
+        pass
+
+
+# functions
+def get_subreddit():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def validate_subreddit():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def get_crawling_depth():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def validate_crawling_depth():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def create_crawler():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def crawl_subreddit():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+def save_data():
+    # TODO: description
+    '''
+    '''
+
+    pass
+
+
+if __name__ == "__main__":
+    pass
